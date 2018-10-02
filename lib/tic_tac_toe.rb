d8242a0a63cafc3ec70bf72274e08828118e1f5d
Robert Chen <robertchen234@gmail.com>
2018-10-02 20:32:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,12 +40,24 @@ class TicTacToe
       return false
     end
   end
-
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
+  
+  def turn(@board)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(@board, index)
+      move(@board, index, current_player(@board))
+      display_board(@board)
+    else
+      turn(@board)
+    end
   end
  
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
+  end
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
   end
 end
